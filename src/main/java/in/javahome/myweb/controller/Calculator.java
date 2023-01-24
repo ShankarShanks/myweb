508c19a22fcdf8bfc65efd537913a59501c01598
Shankar shanks <118338969+ShankarShanks@users.noreply.github.com>
2023-01-24 17:22:09 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -10,9 +10,7 @@ public class Calculator {
 	 * @return int
 	 */
 	public int add(int i, int j){
-		String s = null;
-		s.toUpperCase()
-		return i+j;
+		
 	}
 	public int multiply(int i, int j){
 		return i*j;
